fd52bfba67e87dffcf5844de3c969ee692b59c19
browniecharl <99853094+browniecharl@users.noreply.github.com>
2022-06-10 12:59:40 -0400

    fixed some bugs with server and added some js to public folder

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const path = require('path');
-const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const path = require('path');
 const helpers = require('./utils/helpers')
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+const session = require('express-session');
+
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
 const sess = {
@@ -21,15 +23,14 @@ const sess = {
         db: sequelize,
     }),
 };
-const hbs = exphbs.create({ helpers });
-app.set('view engine', 'handlebars');
+
+app.use(session(sess));
 app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(session(sess));
 
 app.use(routes);
 
